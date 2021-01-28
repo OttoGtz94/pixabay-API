c1afd9da37d95f521e4adcb3eea9b34e9e4cae76
OttoGtz <ogutierrezgz.94@hotmail.com>
2021-01-28 13:13:44 -0600

    Proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Formulario from './components/Formulario';
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [busqueda, guardarBusqueda] = useState('');
+	const [imagenes, guardarImagenes] = useState([]);
+	const [paginaactual, guardarPaginaActual] = useState(1);
+	const [totalpaginas, guardarTotalPaginas] = useState(1);
+
+	useEffect(() => {
+		if (busqueda === '') return;
+
+		const consultarAPI = async () => {
+			const imagenesPorPagina = 10;
+			const key = '20063757-6f71ef882d9bfecfa7c33f251';
+			const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
+
+			const respuesta = await fetch(url);
+			const resultado = await respuesta.json();
+
+			guardarImagenes(resultado.hits);
+
+			// Calcular el total de paginas
+			const calcularTotalPaginas = Math.ceil(
+				resultado.totalHits / imagenesPorPagina
+			);
+			guardarTotalPaginas(calcularTotalPaginas);
+
+			// Ir arriba cuando se de siguiente o anterior
+			const jumbotron = document.querySelector(
+				'.jumbotron'
+			);
+			jumbotron.scrollIntoView({ behavior: 'smooth' });
+		};
+
+		consultarAPI();
+	}, [busqueda, paginaactual]);
+
+	const paginaAnterior = () => {
+		const nuevaPaginaActual = paginaactual - 1;
+		if (nuevaPaginaActual === 0) return;
+		guardarPaginaActual(nuevaPaginaActual);
+	};
+
+	const paginaSiguiente = () => {
+		const nuevaPaginaActual = paginaactual + 1;
+		if (nuevaPaginaActual > totalpaginas) return;
+		guardarPaginaActual(nuevaPaginaActual);
+	};
+
+	return (
+		<div className='container'>
+			<div className='jumbotron'>
+				<p className='lead text-center'>
+					Buscador de Imágenes
+				</p>
+				<Formulario guardarBusqueda={guardarBusqueda} />
+			</div>
+			<div className='row justify-content-center'>
+				<ListadoImagenes imagenes={imagenes} />
+				{paginaactual > 1 ? (
+					<button
+						type='button'
+						className='bbtn btn-info mr-1'
+						onClick={paginaAnterior}
+					>
+						&laquo; Anterior
+					</button>
+				) : null}
+
+				{paginaactual !== totalpaginas ? (
+					<button
+						type='button'
+						className='bbtn btn-info'
+						onClick={paginaSiguiente}
+					>
+						Siguiente &raquo;
+					</button>
+				) : null}
+			</div>
+		</div>
+	);
 }
 
 export default App;
